85ed1f96289d60971457e700d28b49301ad6a111
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-10-30 11:31:13 +0100

    [Oztechan/TraceFit#6] Add Simple GradleBuild and Quality jobs with Slack Integration

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">TraceFit</h1>
+
+<div align="center">
+
+<a href="https://github.com/Oztechan/TraceFit/actions/workflows/main.yml"><img src="https://github.com/Oztechan/TraceFit/actions/workflows/main.yml/badge.svg"></a>
+
+</div>
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     alias(libs.plugins.compose).apply(false)
     alias(libs.plugins.android.application).apply(false)
 }
+
+buildscript {
+    dependencies {
+        // Use the same version in the error
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.17.3")
+    }
+}
+
+allprojects {
+    apply(plugin = "kotlinx-atomicfu")
+}
