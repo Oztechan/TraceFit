1ce95e3037cbcb7f39c07cf65b8803f0e2d5be0f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-11-01 09:17:20 +0100

    [Oztechan/TraceFit#9] Setup Kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
         alias(kotlinMultiplatform).apply(false)
         alias(compose).apply(false)
         alias(androidApplication).apply(false)
+        alias(libs.plugins.kover)
     }
 }
 
@@ -15,5 +16,11 @@ buildscript {
 }
 
 allprojects {
+    apply(plugin = rootProject.libs.plugins.kover.get().pluginId).also {
+        rootProject.dependencies.add("kover", project(path))
+    }
+
+    // todo CMP bug needs to be removed when fixed
+    // https://youtrack.jetbrains.com/issue/KT-41821/Kotlin-1.4.10-ios-target-java.lang.IllegalStateException-failed-to-resolve-Kotlin-library-org.jetbrains.kotlinx-atomicfu-common
     apply(plugin = rootProject.libs.plugins.atomicfu.get().pluginId)
 }
