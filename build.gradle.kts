407df03f46519a8ea42b5f7cc55c8372ac08d5ac
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-11-10 18:36:17 +0100

    [Oztechan/TraceFit#10] Detekt Integration and fixing existing errors (#39)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     libs.plugins.apply {
         alias(jetbrainsCompose) apply false
         alias(androidApplication) apply false
         alias(kotlinMultiplatform) apply false
         alias(kover)
+        alias(detekt)
     }
 }
 
@@ -27,6 +30,37 @@ allprojects {
                 }
             }
             enable()
+        }
+    }
+
+    apply(plugin = rootProject.libs.plugins.detekt.get().pluginId).also {
+        detekt {
+            buildUponDefaultConfig = true
+            allRules = true
+            parallel = true
+            config.from("${rootProject.projectDir}/detekt.yml")
+        }
+        tasks.withType<Detekt> {
+            setSource(files(project.projectDir))
+            exclude("**/build/**")
+            exclude {
+                it.file.relativeTo(projectDir).startsWith(
+                    project.layout.buildDirectory.asFile.get().relativeTo(projectDir)
+                )
+            }
+        }.onEach { detekt ->
+            // skip detekt tasks unless a it is specifically called
+            detekt.onlyIf {
+                gradle.startParameter.taskNames.any { it.contains("detekt") }
+            }
+        }
+
+        tasks.register("detektAll") {
+            dependsOn(tasks.withType<Detekt>())
+        }
+
+        dependencies {
+            detektPlugins(rootProject.libs.common.detektFormatting)
         }
     }
 
