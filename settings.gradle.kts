d4703a796a4f45b9fd8944d8c8fefe8260d71e88
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-01-16 12:55:02 +0200

    [Oztechan/TraceFit#68] Add Gradle build scan and conventional Gradle file names

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
-rootProject.name = "TraceFit"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
@@ -19,4 +18,38 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version ("3.16.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+
+        obfuscation {
+            username { null }
+            hostname { null }
+            ipAddresses { null }
+        }
+    }
+}
+
 include(":composeApp")
+
+rootProject.name = "TraceFit"
+rootProject.updateBuildFileNames()
+
+fun ProjectDescriptor.updateBuildFileNames() {
+    buildFileName = path
+        .drop(1)
+        .replace(":", "-")
+        .dropLastWhile { it != '-' }
+        .plus(name)
+        .plus(".gradle.kts")
+
+    if (children.isNotEmpty()) {
+        children.forEach { it.updateBuildFileNames() }
+    }
+}
