e7475bb24e2ffcd919266c1c501ff529aa83a991
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-02-26 19:18:50 +0100

    [Oztechan/TraceFit#92] Remove atomicfu

--- TraceFit.gradle.kts
+++ TraceFit.gradle.kts
@@ -12,14 +12,6 @@ plugins {
     }
 }
 
-// todo CMP bug needs to be removed when fixed
-// https://youtrack.jetbrains.com/issue/KT-41821/Kotlin-1.4.10-ios-target-java.lang.IllegalStateException-failed-to-resolve-Kotlin-library-org.jetbrains.kotlinx-atomicfu-common
-buildscript {
-    dependencies {
-        classpath(libs.classpaths.atomicfu)
-    }
-}
-
 allprojects {
     apply(plugin = rootProject.libs.plugins.kover.get().pluginId).also {
         koverMerged {
@@ -65,8 +57,4 @@ allprojects {
             detektPlugins(rootProject.libs.common.detektFormatting)
         }
     }
-
-    // todo CMP bug needs to be removed when fixed
-    // https://youtrack.jetbrains.com/issue/KT-41821/Kotlin-1.4.10-ios-target-java.lang.IllegalStateException-failed-to-resolve-Kotlin-library-org.jetbrains.kotlinx-atomicfu-common
-    apply(plugin = rootProject.libs.plugins.atomicfu.get().pluginId)
 }
