2a47b3e7b386b3243fcf460760cfe94ddeecc726
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-05-15 19:53:04 +0200

    [Oztechan/TraceFit#130] Update kotlin to 1.9.23 (#131)
    * [Oztechan/TraceFit#130] Update kotlin to 1.9.23
    
    * [Oztechan/TraceFit#130] Update kotlin to 1.9.23
    
    * [Oztechan/TraceFit#130] Update kotlin to 1.9.23

--- client/src/commonMain/kotlin/com/oztechan/tracefit/client/MainView.kt
+++ client/src/commonMain/kotlin/com/oztechan/tracefit/client/MainView.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -32,7 +33,7 @@ fun MainView() {
             }
             AnimatedVisibility(showImage) {
                 Image(
-                    painterResource("compose-multiplatform.xml"),
+                    painterResource(DrawableResource("compose-multiplatform")),
                     null
                 )
             }
