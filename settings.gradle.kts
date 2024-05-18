2b99224c0628335868b38d59cc1aaa7d0253e92a
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-05-18 20:46:41 +0200

    [Oztechan/TraceFit#132] Move new setup of compose resources

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
         mavenCentral()
@@ -13,6 +14,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
