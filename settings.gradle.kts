b90914c204fbce41bdd7ba3f1932e14831a827ac
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-05-19 10:00:44 +0200

    [Oztechan/TraceFit#134] Remove unnecessary settings.gradle urls

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,8 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
-        mavenCentral()
     }
 }
 
@@ -14,7 +12,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
