cc8f7c22d5d72a7b437dc4e6fa5f398b7f3e4fc7
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2026-07-27 23:44:34 +0200

    [Oztechan/TraceFit#11] Bump compile and target SDK to 36

--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -13,9 +13,9 @@ object ProjectSettings {
 
     const val PROJECT_ID = "com.oztechan.tracefit"
 
-    const val COMPILE_SDK_VERSION = 35
+    const val COMPILE_SDK_VERSION = 36
     const val MIN_SDK_VERSION = 24
-    const val TARGET_SDK_VERSION = 35
+    const val TARGET_SDK_VERSION = 36
 
     val JAVA_VERSION = JavaVersion.VERSION_21
 
